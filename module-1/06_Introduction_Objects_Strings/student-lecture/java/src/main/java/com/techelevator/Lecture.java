90a23dd16db7f7756a5f2b8ac9a133ae159c6313
David P-C <david.pc@techelevator.com>
2020-09-21 08:34:30 -0400

    Added student-lecture for m1-06.

---
+++ module-1/06_Introduction_Objects_Strings/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,73 @@
+package com.techelevator;
+
+public class Lecture {
+
+	//1. Return any String
+	public String returnsAString() {
+		return null;
+	}
+
+	//2. getInputLength("abc") -> 3
+	public int getInputLength(String input) {
+		return -1;
+	}
+
+	//3. getFirst3Letters("abcdef") -> "abc"
+	public String getFirst3Letters(String input) {
+		return new String();
+	}
+
+	//4. getLast3Letters("abcdef") -> "def"
+	public String getLast3Letters(String input) {
+		return "";
+	}
+
+	//5. removeFirstLetter("abcdef") -> "bcdef"
+	public String removeFirstLetter(String input) {
+		return "";
+	}
+
+	//6. locationOfFirstCat("I have a catalog of cats.") -> 9
+	public int locationOfFirstCat(String stringWithCat) {
+		return Integer.MAX_VALUE;
+	}
+
+	//7. locationOfLastCat("I have a catalog of cats.") -> 20
+	public int locationOfLastCat(String stringWithCat) {
+		return Integer.MIN_VALUE;
+	}
+
+	//8. getFifthCharacter("abcdef") -> 'e'
+	public char getFifthCharacter(String input) {
+		return 'x';
+	}
+
+	//9. isCareful("I don't think Voldemort is real.") -> "No"
+	//   isCareful("Beware of He Who Must Not Be Named.") -> "Yes"
+	public String isCareful(String statement) {
+		return "Yes";
+	}
+
+	//10. roundOnBothEnds("Ohio") -> true
+	//    roundOnBothEnds("Oklahoma") -> false
+	public boolean roundOnBothEnds(String name) {
+		return false;
+	}
+
+	//11. sayItLouder("Look out!") -> "LOOK OUT!"
+	public String sayItLouder(String phrase) {
+		return phrase;
+	}
+
+	//12. princeToFrog("The prince sat by the pond.") -> "The frog sat by the pond. "
+	public String princeToFrog(String story) {
+		return "";
+	}
+
+	//13. isDarthVader("Anakin", "Skywalker") -> true
+	public boolean isDarthVader(String firstName, String lastName) {
+		return false;
+	}
+
+
+}
---
+++ module-1/06_Introduction_Objects_Strings/student-lecture/java/src/test/java/com/techelevator/LectureTest.java
@@ -0,0 +1,89 @@
+package com.techelevator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LectureTest {
+
+    Lecture sut = new Lecture();
+
+
+    @Test
+    public void test1_returnsAString() {
+        assertNotNull(sut.returnsAString());
+    }
+
+    @Test
+    public void test2_getInputLength() {
+        assertEquals(3, sut.getInputLength("abc"));
+    }
+
+    @Test
+    public void test3_getFirst3Letters() {
+        assertEquals("abc", sut.getFirst3Letters("abcdef"));
+    }
+
+    @Test
+    public void test4_getLast3Letters() {
+        assertEquals("def", sut.getLast3Letters("abcdef"));
+    }
+
+    @Test
+    public void test5_removeFirstLetter() {
+        assertEquals("bcdef", sut.removeFirstLetter("abcdef"));
+    }
+
+    @Test
+    public void test6_locationOfFirstCat() {
+        assertEquals(9, sut.locationOfFirstCat("I have a catalog of cats."));
+    }
+
+    @Test
+    public void test7_locationOfLastCat() {
+        assertEquals(20, sut.locationOfLastCat("I have a catalog of cats."));
+    }
+
+    @Test
+    public void test8_getFifthCharacter() {
+        assertEquals('e', sut.getFifthCharacter("abcdef"));
+    }
+
+    @Test
+    public void test9_isCareful() {
+        assertEquals("No", sut.isCareful("I don't think Voldemort is real."));
+        assertEquals("Yes", sut.isCareful("Beware of He Who Must Not Be Named."));
+
+    }
+
+    @Test
+    public void test10_roundOnBothEnds() {
+        assertTrue(sut.roundOnBothEnds("Ohio"));
+        assertFalse(sut.roundOnBothEnds("Oklahoma"));
+    }
+
+    @Test
+    public void test11_sayItLouder() {
+        assertEquals("LOOK OUT!", sut.sayItLouder("Look out!"));
+    }
+
+    @Test
+    public void test12_princeToFrog() {
+        assertEquals("The frog sat by the pond.", sut.princeToFrog("The prince sat by the pond."));
+    }
+
+    @Test
+    public void test13_isDarthVader() {
+        assertTrue(sut.isDarthVader("Anakin", "Skywalker"));
+    }
+
+
+
+
+
+
+
+
+
+}
