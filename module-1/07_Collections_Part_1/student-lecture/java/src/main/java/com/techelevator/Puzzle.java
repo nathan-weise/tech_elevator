4d4c1fe7d457d887b6740395030fd1cb08a226de
David P-C <david.pc@techelevator.com>
2020-09-22 08:27:13 -0400

    Added student-lecture for m1-07.

---
+++ module-1/07_Collections_Part_1/student-lecture/java/src/main/java/com/techelevator/Puzzle.java
@@ -0,0 +1,28 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+public class Puzzle {
+
+    public static void main(String[] args) {
+        System.out.print("Enter text: ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+        int half = alphabet.length() / 2;
+        String rotated = alphabet.substring(half) + alphabet.substring(0, half);
+
+        char[] output = new char[input.length()];
+        for (int i = 0; i < input.length(); i++) {
+            char letter = input.charAt(i);
+            int position = alphabet.indexOf(letter);
+            if (position > -1) {
+                output[i] = rotated.charAt(position);
+            } else {
+                output[i] = letter;
+            }
+        }
+        System.out.println(new String(output));
+    }
+}
