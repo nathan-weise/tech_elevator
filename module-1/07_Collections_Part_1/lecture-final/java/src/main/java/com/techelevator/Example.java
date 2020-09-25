185754391dfb5f213263ef6a461e3223e7b68b29
David P-C <david.pc@techelevator.com>
2020-09-25 13:33:10 -0400

    Updated lecture-final for m1-07.

---
+++ module-1/07_Collections_Part_1/lecture-final/java/src/main/java/com/techelevator/Example.java
@@ -0,0 +1,44 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        List<String> iceCreamTypes = new ArrayList<>();
+        iceCreamTypes.add("cow milk");
+        iceCreamTypes.add("soy milk");
+        iceCreamTypes.add("almond milk");
+        iceCreamTypes.add("coconut milk");
+
+        for (int i = 0; i < iceCreamTypes.size(); i++) {
+            String variety = iceCreamTypes.get(i);
+            variety = variety.replace("milk", "");
+            System.out.println(variety);
+        }
+
+        iceCreamTypes.add("cashew milk");
+
+        for (int i = 0; i < iceCreamTypes.size(); i++) {
+            String variety = iceCreamTypes.get(i);
+            System.out.println(variety);
+        }
+
+        for (String variety : iceCreamTypes) {
+            System.out.println(variety);
+        }
+
+        List<String> iceCreamFlavors = new ArrayList<>(Arrays.asList("chocolate", "vanilla"));
+
+        for (String flavor : iceCreamFlavors) {
+            for (String variety : iceCreamTypes) {
+                System.out.println(flavor + " " + variety);
+            }
+        }
+
+
+    }
+}
