fef1664b70f64e3072d68d1b44873bf8104da77d
David P-C <david.pc@techelevator.com>
2020-10-20 08:31:55 -0400

    Added student-lecture for m2-07.

---
+++ module-2/07_Database_Connectivity_DAO/student-lecture/java/src/main/java/com/techelevator/SpringJDBCExample.java
@@ -0,0 +1,24 @@
+package com.techelevator;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+/* Working with JDBC entails a lot of boilerplate code which can become 
+ * tedious and error prone. The Spring JBDC framework can help eliminate 
+ * much of the boilerplate code, and limit the tedium and mistakes. */
+public class SpringJDBCExample {
+
+	public static void main(String[] args) {
+		
+		/* This datasource will be used for creating connections to the database.
+		 * Below, we provide the information required to make database connections */
+		BasicDataSource dvdstoreDataSource = new BasicDataSource();
+		dvdstoreDataSource.setUrl("jdbc:postgresql://localhost:5432/dvdstore");
+		dvdstoreDataSource.setUsername("postgres");
+		dvdstoreDataSource.setPassword("postgres1"); //Don't make this a habit--use an environment variable or external config file.
+
+
+		
+	}
+}
