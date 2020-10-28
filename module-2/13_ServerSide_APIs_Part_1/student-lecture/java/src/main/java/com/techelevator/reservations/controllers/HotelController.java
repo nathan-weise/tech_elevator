0148dd8dab7e5df0b7e362a08d64310cfb84f98d
David P-C <david.pc@techelevator.com>
2020-10-28 08:36:56 -0400

    Added student-lecture for m2-13.

---
+++ module-2/13_ServerSide_APIs_Part_1/student-lecture/java/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -0,0 +1,24 @@
+package com.techelevator.reservations.controllers;
+
+import com.techelevator.reservations.dao.HotelDAO;
+import com.techelevator.reservations.dao.MemoryHotelDAO;
+import com.techelevator.reservations.dao.MemoryReservationDAO;
+import com.techelevator.reservations.dao.ReservationDAO;
+import com.techelevator.reservations.models.Hotel;
+import com.techelevator.reservations.models.Reservation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class HotelController {
+
+    private final HotelDAO hotelDAO;
+    private final ReservationDAO reservationDAO;
+
+    public HotelController() {
+        this.hotelDAO = new MemoryHotelDAO();
+        this.reservationDAO = new MemoryReservationDAO(hotelDAO);
+    }
+
+
+}
