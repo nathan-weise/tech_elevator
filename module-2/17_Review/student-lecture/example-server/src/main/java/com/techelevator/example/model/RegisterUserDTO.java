db0773ebadbc8e9a8df03156727d3586c03098a8
David P-C <david.pc@techelevator.com>
2020-11-03 11:17:37 -0500

    Added student-lecture for m2-17.

---
+++ module-2/17_Review/student-lecture/example-server/src/main/java/com/techelevator/example/model/RegisterUserDTO.java
@@ -0,0 +1,37 @@
+package com.techelevator.example.model;
+
+import javax.validation.constraints.NotEmpty;
+
+public class RegisterUserDTO {
+
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
