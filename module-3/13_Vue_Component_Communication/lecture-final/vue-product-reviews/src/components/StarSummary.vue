20d887318f77042b4121c4233b75d9c5cbe4df53
David P-C <david.pc@techelevator.com>
2020-11-25 13:14:44 -0500

    Added lecture-final for m3-13.

---
+++ module-3/13_Vue_Component_Communication/lecture-final/vue-product-reviews/src/components/StarSummary.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="well" v-on:click="updateFilter()">
+    <span class="amount">{{ numberOfReviews }}</span>
+    {{ rating }} Star Review{{ numberOfReviews === 1 ? '' : 's' }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'star-summary',
+  props: ['rating', 'favoriteHoliday'],
+  methods: {
+    updateFilter() {
+      this.$store.commit('UPDATE_FILTER', parseInt(this.rating));
+    }
+  },
+  computed: {
+    numberOfReviews() {
+      const reviews = this.$store.state.reviews;
+      return reviews.reduce((currentCount, review) => {
+        return currentCount + (review.rating === parseInt(this.rating) ? 1 : 0);
+      }, 0);
+    }
+  }
+};
+</script>
+
