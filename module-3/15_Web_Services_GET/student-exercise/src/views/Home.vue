500dd54ed3d2ca3f82db1283d85c1ea19f94c13a
David P-C <david.pc@techelevator.com>
2020-12-02 08:36:44 -0500

    Added student-exercise for m3-15.

---
+++ module-3/15_Web_Services_GET/student-exercise/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h1>Topic Message Board</h1>
+    <topic-list v-bind:topics="allTopics"/>
+  </div>
+</template>
+
+<script>
+import TopicList from '@/components/TopicList.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    TopicList
+  },
+  data() {
+    return {
+      allTopics: []
+    }
+  }
+}
+</script>
